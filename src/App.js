9bb94c7e481c74fcdd7e9f42f84ca9566f8d037d
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-04-12 10:20:32 -0300

    Add example with two instances of AlloyEditorComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import AlloyEditorComponent from './alloyeditor';
 
 class App extends Component {
-  render() {
-    return <AlloyEditorComponent container="editable" />;
-  }
+    render() {
+        return (
+            <Fragment>
+                <AlloyEditorComponent container='title'>
+                    <h1>AlloyEditor will make this content editable</h1>
+                </AlloyEditorComponent>
+                <AlloyEditorComponent container='editable'>
+                    <p>
+                        To install React, follow the instructions on <a href="https://github.com/facebook/react/">GitHub</a>.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel metus nunc. Maecenas rhoncus congue faucibus. Sed finibus ultrices turpis. Mauris nulla ante, aliquam a euismod ut, scelerisque nec sem. Nam dapibus ac nulla non ullamcorper. Sed vestibulum a velit non lobortis. Proin sit amet imperdiet urna. Aenean interdum urna augue, vel mollis tortor dictum vitae. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris vitae suscipit magna.
+                    </p>
+                </AlloyEditorComponent>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
